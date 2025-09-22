1ad4b4ea46d17c7cc84230907b6e9c53dd1844d2
MMurdock17 <mattiemurdock1@gmail.com>
2025-09-21 22:41:33 -0400

    More on projectiles, player attacks

--- 2D Prototype/Assets/Scripts/Projectile.cs
+++ 2D Prototype/Assets/Scripts/Projectile.cs
@@ -4,9 +4,10 @@ public class Pineapple : MonoBehaviour
 {
  
 
-[SerializeField] private float speed;
+[SerializeField] private float speed = 10f;
 private float direction;
 private bool hit;
+private float lifetime;
 
 private Animator anim;
 private BoxCollider2D boxCollider;
@@ -20,8 +21,12 @@ private void Awake()
 private void Update()
 {
 	if (hit) return;
+
 	float movementSpeed = speed * Time.deltaTime * direction;
 	transform.Translate(movementSpeed, 0, 0);
+
+	lifetime += Time.deltaTime;
+	if (lifetime > 5) gameObject.SetActive(false);
 }
 
 private void OnTriggerEnter2D(Collider2D collision)
@@ -33,10 +38,18 @@ private void OnTriggerEnter2D(Collider2D collision)
 
 public void SetDirection(float _direction)
 {
+	lifetime = 0;
 	direction = _direction;
 	gameObject.SetActive(true);
 	hit = false;
 	boxCollider.enabled = true;
+
+	float localScaleX = transform.localScale.x;
+	if (Mathf.Sign(localScaleX) != _direction)
+		localScaleX = -localScaleX;
+
+		transform.localScale = new Vector3(localScaleX, transform.localScale.y, transform.localScale.z);
+
 }
 
 private void Deactivate()
