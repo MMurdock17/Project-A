71f10ec2722de9c100cf62889d75831489bf3ca8
MMurdock17 <mattiemurdock1@gmail.com>
2025-09-28 16:42:28 -0400

    Adding assets needed for game over screen

--- 2D Prototype/Assets/Scripts/Player/PlayerRespawn.cs
+++ 2D Prototype/Assets/Scripts/Player/PlayerRespawn.cs
@@ -2,21 +2,22 @@ using UnityEngine;
 
 public class PlayerRespawn : MonoBehaviour
 {
- /*   private Health playerHealth;
+    private Health playerHealth;
     private Vector3 startPosition;
+    private UIManager uiManager;
 
     private void Awake()
     {
         playerHealth = GetComponent<Health>();
         startPosition = transform.position;
+        uiManager = FindObjectOfType<UIManager>();
     }
     public void Respawn()
     {
-    
+        uiManager.GameOver();
         transform.position = startPosition;
 
-    playerHealth.Respawn();
 
     Camera.main.GetComponent<CameraController>().NewRoom(transform.parent);
-    } */
+    } 
 }
