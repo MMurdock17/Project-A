dadc8640f6fbc8f76ec40c20a8469e60b360592d
MMurdock17 <mattiemurdock1@gmail.com>
2025-09-27 22:44:11 -0400

    Polishing enemy patrolling

--- 2D Prototype/Assets/Scripts/Enemy/EnemyPatrol.cs
+++ 2D Prototype/Assets/Scripts/Enemy/EnemyPatrol.cs
@@ -12,19 +12,63 @@ public class EnemyPatrol : MonoBehaviour
     [Header ("Movement parameters")]
     [SerializeField] private float speed;
     private Vector3 initScale;
+    private bool movingLeft;
+
+    [Header ("Idle Behavior")]
+    [SerializeField] private float idleDuration;
+    private float idleTimer;
+
+    [Header ("Enemy Animator")]
+    [SerializeField] private Animator anim;
 
     private void Awake()
     {
         initScale = enemy.localScale;
     }
 
+    private void OnDisable()
+    {
+        anim.SetBool("moving", false);
+    }
+
     private void Update()
     {
-        MoveInDirection(1);
+        if (movingLeft)
+        {
+            if (enemy.position.x >= leftEdge.position.x)
+                MoveInDirection(-1);
+            else
+            {
+                DirectionChange();
+            }
+        }
+        else
+        {
+            if (enemy.position.x <= rightEdge.position.x)
+                MoveInDirection(1);
+        
+            else
+            {
+                DirectionChange();
+            }
+        }
+    }
+
+    private void DirectionChange()
+    {
+        idleTimer += Time.deltaTime;
+        anim.SetBool("moving", false);
+        
+        if (idleTimer > idleDuration)
+            movingLeft = !movingLeft;
     }
 
     private void MoveInDirection(int _direction)
     {
+        idleTimer = 0;
+
+        anim.SetBool("moving", true);
+
         enemy.localScale = new Vector3(Mathf.Abs(initScale.x) * _direction, initScale.y, initScale.z);
 
         enemy.position = new Vector3(enemy.position.x + Time.deltaTime * _direction * speed, enemy.position.y, enemy.position.z);
